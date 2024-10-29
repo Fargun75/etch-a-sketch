550d412dc66a5c4ac7cfaa07db199c5c723e8cb4
Gunnar Farup <113795596+Fargun75@users.noreply.github.com>
2024-10-29 15:00:25 +0100

    Add a loop to create pads on the sketchpad

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("hello!");
+let sketchPad = document.querySelector('.sketchPad');
+
+for (i = 0; i < 4; i++) {
+    let element = document.createElement('div');
+    element.classList.add('pad');
+    for (j = 0; j < 4; j++) {
+        let newPad = document.createElement('div');
+        newPad.classList.add('newpad');
+        element.appendChild(newPad);
+    }
+    sketchPad.appendChild(element);
+}
+
