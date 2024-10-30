33dbdcad6081fc3898ee2659f9db67aa7aff9278
Gunnar Farup <113795596+Fargun75@users.noreply.github.com>
2024-10-30 09:35:20 +0100

    Make new grid work, but must be made simpler

--- js/script.js
+++ js/script.js
@@ -17,3 +17,25 @@ function hovered(e) {
 }
 const hover = document.querySelectorAll('.newpad');
 hover.forEach((pad) => pad.addEventListener('mouseover', hovered));
+
+
+let gridSize = document.querySelector('#size');
+gridSize.addEventListener('click', changeSize);
+function changeSize() {
+    let userInput = prompt("How many square per side (max 100)?");
+    sketchPad.textContent = "";
+    for (i = 0; i < userInput; i++) {
+        let element = document.createElement('div');
+        element.classList.add('pad');
+        for (j = 0; j < userInput; j++) {
+            let newPad = document.createElement('div');
+            newPad.classList.add('newpad');
+            element.appendChild(newPad);
+        }
+        sketchPad.appendChild(element);
+    }
+    const hover = document.querySelectorAll('.newpad');
+    hover.forEach((pad) => pad.addEventListener('mouseover', hovered));
+}
+
+
